fc93cdfc620b66a12f15d71458a8c919aa09c6c6
Irwing Naranjo <khalisser@gmail.com>
2023-05-03 13:16:58 -0400

    fix name reserved page test

--- __tests__/pages/apis/hello.spec.js
+++ __tests__/pages/apis/hello.spec.js
@@ -1,11 +1,10 @@
-// @ts-ignore
 import handler from '@api/hello'
 
 describe('hello api', () => {
   it('should return hello world', async () => {
-    const req: any = {}
+    const req = {}
 
-    const res: any = {
+    const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn().mockReturnThis()
     }
@@ -14,5 +13,5 @@ describe('hello api', () => {
 
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.json).toHaveBeenCalledWith({ name: 'John Doe' })
-  });
-});
+  })
+})
--- src/pages/test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Title from '@/components/title';
-
-const Test = () => {
-  return <Title title="Test" />;
-};
-
-export default Test;
